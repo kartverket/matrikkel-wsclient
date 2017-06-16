792c331dd2d077f5b465c6576bd91725ee3c09b6
Leif Eirik Lislegård <lislei@statkart.no>
2017-06-16 15:27:48 +0100

    MAT-14044 verkty for validering av innsendingsformat
    [git-p4: depot-paths = "//matrikkel/matrikkel-wsclient/trunk/": change = 79548]

---
+++ src/main/java/no/statkart/wsclient/grunnbokv2/innsending/validate/InnsendingValidator.java
@@ -0,0 +1,87 @@
+package no.statkart.wsclient.grunnbokv2.innsending.validate;
+
+import no.statkart.wsclient.grunnbokv2.innsending.InnsendingServiceMapper;
+import no.statkart.wsclient.grunnbokv2.innsending.domene.Forsendelse;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.namespace.QName;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @since 3.12
+ */
+public class InnsendingValidator {
+
+   private static final JAXBContext jaxbContext;
+   private static final Marshaller marshaller;
+   private static Schema schema;
+
+   static {
+      try {
+         jaxbContext = JAXBContext.newInstance("no.kartverket.grunnbok.wsapi.v2.domain.innsending");
+         marshaller = jaxbContext.createMarshaller();
+         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+         marshaller.setSchema(null);
+         SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+         schema = schemaFactory.newSchema(new StreamSource(Thread.currentThread().getContextClassLoader().getResource("innsending/schema/innsending.xsd").openStream()));
+      } catch (JAXBException | SAXException | IOException e) {
+         throw new RuntimeException(e);
+      }
+   }
+
+
+   public static void validateForsendelseXml(Forsendelse forsendelse) throws JAXBException, IOException, SAXException {
+      InnsendingServiceMapper mapper = InnsendingServiceMapper.getInstance();
+      no.kartverket.grunnbok.wsapi.v2.domain.innsending.Forsendelse jaxbForsendelse
+            = mapper.getMapping().d2w(forsendelse, no.kartverket.grunnbok.wsapi.v2.domain.innsending.Forsendelse.class);
+
+      JAXBElement<no.kartverket.grunnbok.wsapi.v2.domain.innsending.Forsendelse> rootElement =
+            new JAXBElement<>(
+                  new QName("http://kartverket.no/grunnbok/wsapi/v2/domain/innsending", "forsendelse"),
+                  no.kartverket.grunnbok.wsapi.v2.domain.innsending.Forsendelse.class,
+                  jaxbForsendelse);
+
+      ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+      marshaller.marshal(rootElement, bos);
+      byte[] xmlBytes = bos.toByteArray();
+      try {
+         schema.newValidator().validate(new StreamSource(new ByteArrayInputStream(xmlBytes)));
+      } catch (SAXParseException e) {
+         BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(xmlBytes), StandardCharsets.UTF_8));
+         StringBuilder sb = new StringBuilder();
+         sb.append("An ").append(e.getClass().getName()).append(" was caught:\n");
+         sb.append(e.toString());
+         String readLine;
+         int lineNumber = 1;
+         while ((readLine = reader.readLine()) != null) {
+            sb.append('\t');
+            sb.append(readLine);
+            sb.append('\n');
+            if (e.getLineNumber() == lineNumber) {
+               for (int j = 1; j < e.getColumnNumber(); ++j) {
+                  sb.append(' ');
+               }
+               sb.append("^--- HERE ---");
+               sb.append('\n');
+               break;
+            } else {
+               ++lineNumber;
+            }
+         }
+         throw new IllegalArgumentException(sb.toString());
+//         fail(sb.toString());
+      }
+   }
+
+}
---
+++ src/test/java/no/statkart/wsclient/grunnbokv2/innsending/validate/InnsendingValidatorTest.java
@@ -0,0 +1,26 @@
+package no.statkart.wsclient.grunnbokv2.innsending.validate;
+
+import no.statkart.wsclient.grunnbokv2.innsending.domene.Forsendelse;
+import no.statkart.wsclient.grunnbokv2.innsending.domene.builder.forsendelse.ForsendelseBuilder;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.ThrowableAssert;
+import org.testng.annotations.Test;
+
+public class InnsendingValidatorTest {
+
+
+   @Test
+   public void testTomForsendelseV2GirValideringsfeil() {
+      final ForsendelseBuilder forsendelseBuilder = ForsendelseBuilder.aForsendelse();
+      final Forsendelse forsendelse = forsendelseBuilder.build();
+
+      Assertions.assertThatThrownBy(new ThrowableAssert.ThrowingCallable() {
+         @Override
+         public void call() throws Throwable {
+            InnsendingValidator.validateForsendelseXml(forsendelse);
+         }
+      }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("\"http://kartverket.no/grunnbok/wsapi/v2/domain/innsending\":forsendelsesreferanse}' is expected.");
+   }
+
+}
