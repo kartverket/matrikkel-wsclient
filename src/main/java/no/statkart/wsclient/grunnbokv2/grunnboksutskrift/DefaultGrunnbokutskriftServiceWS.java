5ed4aef1d8c26e3ce9d95ffaa7b16d401d772b1c
Alexander Solstad Larsen <larale@kartverket.no>
2018-06-29 07:50:18 +0100

    MAT-14495 Utvidet wsclient med ikke-intern grunnboksutskrifttjeneste
    [git-p4: depot-paths = "//matrikkel/matrikkel-wsclient/trunk/": change = 87139]

---
+++ src/main/java/no/statkart/wsclient/grunnbokv2/grunnboksutskrift/DefaultGrunnbokutskriftServiceWS.java
@@ -0,0 +1,48 @@
+package no.statkart.wsclient.grunnbokv2.grunnboksutskrift;
+
+import no.kartverket.grunnbok.wsapi.v2.domain.basistyper.GrunnbokContext;
+import no.kartverket.grunnbok.wsapi.v2.domain.register.registerenhet.RegisterenhetId;
+import no.kartverket.grunnbok.wsapi.v2.exception.ServiceException;
+import no.kartverket.grunnbok.wsapi.v2.service.grunnboksutskrift.GrunnboksutskriftService;
+import no.kartverket.grunnbok.wsapi.v2.service.grunnboksutskrift.GrunnboksutskriftServiceWS;
+import no.statkart.wsclient.WebServiceBuilder;
+
+/**
+ *
+ */
+public class DefaultGrunnbokutskriftServiceWS implements GrunnboksutskriftService {
+
+   private static GrunnboksutskriftServiceWS grunnboksutskriftServiceWS;
+
+   private final GrunnboksutskriftService service;
+
+   public DefaultGrunnbokutskriftServiceWS(String brukernavn, String passord, String endpointUrl) {
+      if(grunnboksutskriftServiceWS == null){
+         synchronized (this) {
+            if(grunnboksutskriftServiceWS == null){
+               grunnboksutskriftServiceWS = new GrunnboksutskriftServiceWS();
+            }
+         }
+      }
+
+      service = WebServiceBuilder.builderv2(grunnboksutskriftServiceWS.getGrunnboksutskriftServicePort(), GrunnboksutskriftService.class)
+            .withBruker(brukernavn)
+            .withPassord(passord)
+            .withEndpointUrl(endpointUrl)
+            .doCreateProxy()
+            .build();
+   }
+
+   @Override
+   public String ubekreftetHistoriskGrunnboksutskrift(RegisterenhetId registerenhetId, GrunnbokContext grunnbokContext) throws ServiceException {return service.ubekreftetHistoriskGrunnboksutskrift(registerenhetId, grunnbokContext);}
+
+   @Override
+   public byte[] ubekreftetHistoriskGrunnboksutskriftPdf(RegisterenhetId registerenhetId, GrunnbokContext grunnbokContext) throws ServiceException {return service.ubekreftetHistoriskGrunnboksutskriftPdf(registerenhetId, grunnbokContext);}
+
+   @Override
+   public String ubekreftetGrunnboksutskrift(RegisterenhetId registerenhetId, GrunnbokContext grunnbokContext) throws ServiceException {return service.ubekreftetGrunnboksutskrift(registerenhetId, grunnbokContext);}
+
+   @Override
+   public byte[] ubekreftetGrunnboksutskriftPdf(RegisterenhetId registerenhetId, GrunnbokContext grunnbokContext) throws ServiceException {return service.ubekreftetGrunnboksutskriftPdf(registerenhetId, grunnbokContext);}
+
+}
