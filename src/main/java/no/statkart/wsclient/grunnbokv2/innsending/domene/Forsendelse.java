2cb53f6ad9072f94e6a2df2a8c9f875c0ace8c1c
Adrian Haugli Rovelstad <rovadr@kartverket.no>
2021-06-08 13:52:05 +0200

    MAT-18459: Endret flagget "linkTilUtskrifter" til å være default true

--- src/main/java/no/statkart/wsclient/grunnbokv2/innsending/domene/Forsendelse.java
+++ src/main/java/no/statkart/wsclient/grunnbokv2/innsending/domene/Forsendelse.java
@@ -7,51 +7,51 @@ import java.util.List;
 
 public class Forsendelse {
 
-   private String forsendelsesreferanse;
-   // MAT-18455 Flagget må være false inntil MAT-18459 henter utskrift fra link
-   private final Boolean linkTilUtskrifter = false;
-   private SignertMelding signertMelding;
-   private UsignertMelding usignertMelding;
-   private List<Signatur> ikkeDigitaleSignaturer = Lists.newArrayList();
+    private String forsendelsesreferanse;
+    // MAT-18459 Flagget er satt tilbake til å være true.
+    private final Boolean linkTilUtskrifter = true;
+    private SignertMelding signertMelding;
+    private UsignertMelding usignertMelding;
+    private List<Signatur> ikkeDigitaleSignaturer = Lists.newArrayList();
 
-   public void setIkkeDigitaleSignaturer(List<Signatur> ikkeDigitaleSignaturer) {
-      this.ikkeDigitaleSignaturer = ikkeDigitaleSignaturer;
-   }
+    public void setIkkeDigitaleSignaturer(List<Signatur> ikkeDigitaleSignaturer) {
+        this.ikkeDigitaleSignaturer = ikkeDigitaleSignaturer;
+    }
 
-   public List<Signatur> getIkkeDigitaleSignaturer() {
-      return ikkeDigitaleSignaturer;
-   }
+    public List<Signatur> getIkkeDigitaleSignaturer() {
+        return ikkeDigitaleSignaturer;
+    }
 
-   public String getForsendelsesreferanse() {
-      return forsendelsesreferanse;
-   }
+    public String getForsendelsesreferanse() {
+        return forsendelsesreferanse;
+    }
 
-   public void setForsendelsesreferanse(String forsendelsesreferanse) {
-      this.forsendelsesreferanse = forsendelsesreferanse;
-   }
+    public void setForsendelsesreferanse(String forsendelsesreferanse) {
+        this.forsendelsesreferanse = forsendelsesreferanse;
+    }
 
-   public Boolean getLinkTilUtskrifter() {
+    public Boolean getLinkTilUtskrifter() {
         return linkTilUtskrifter;
-   }
+    }
 
-   public void setLinkTilUtskrifter(Boolean linkTilUtskrifter) {
-   // Trenger setter for mapping, men flagget skal alltid være true
-   }
+    public void setLinkTilUtskrifter(Boolean linkTilUtskrifter) {
+        // Trenger setter for mapping, men flagget skal alltid være true
+    }
 
     public SignertMelding getSignertMelding() {
-      return signertMelding;
-   }
+        return signertMelding;
+    }
 
-   public void setSignertMelding(SignertMelding signertMelding) {
-      this.signertMelding = signertMelding;
-   }
+    public void setSignertMelding(SignertMelding signertMelding) {
+        this.signertMelding = signertMelding;
+    }
 
-   public UsignertMelding getUsignertMelding() {
-      return usignertMelding;
-   }
+    public UsignertMelding getUsignertMelding() {
+        return usignertMelding;
+    }
 
-   public void setUsignertMelding(UsignertMelding usignertMelding) {
-      this.usignertMelding = usignertMelding;
-   }
+    public void setUsignertMelding(UsignertMelding usignertMelding) {
+        this.usignertMelding = usignertMelding;
+    }
 
 }
