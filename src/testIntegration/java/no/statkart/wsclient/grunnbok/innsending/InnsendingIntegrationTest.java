908c7a628942a059faef5d4e2c39584f1504b3a8
Leif Lislegard <lislei@kartverket.no>
2023-08-10 11:07:29 +0200

    TH-726 Splitter feilende test InnsendingIntegrationTest.testHentStatus

--- src/testIntegration/java/no/statkart/wsclient/grunnbok/innsending/InnsendingIntegrationTest.java
+++ src/testIntegration/java/no/statkart/wsclient/grunnbok/innsending/InnsendingIntegrationTest.java
@@ -6,6 +6,7 @@ import no.statkart.wsclient.grunnbokv2.innsending.InnsendingServiceWS;
 import no.statkart.wsclient.grunnbokv2.innsending.domene.Forsendelsesstatus;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,20 +18,31 @@ public class InnsendingIntegrationTest {
     public void testHentStatus() {
         Forsendelsesstatus forsendelsesstatus = innsendingServiceWS.hentStatus("841822389");
         assertThat(forsendelsesstatus).isNotNull();
+        assertThat(forsendelsesstatus.getInnsendingId())
+            .as("Informasjon blankes for gamle innsendinger").isNull();
+    }
+
+    /**
+     * Manuell test da tinglysingsinformasjon og signerte utskrifter fjernes automatisk etter en viss tid.
+     */
+    @Ignore
+    @Test
+    public void testHentStatus_signerteGrunnboksutskrifter() {
+        Forsendelsesstatus forsendelsesstatus = innsendingServiceWS.hentStatus("841822389");
+        assertThat(forsendelsesstatus).isNotNull();
         assertThat(forsendelsesstatus.getTinglysingsinformasjon()).isNotNull();
         assertThat(forsendelsesstatus.getTinglysingsinformasjon().getSignerteGrunnboksutskrifter())
             .isNotNull().hasSize(2);
-
     }
 
     @BeforeTest
     public void setUp() {
         final IntegrationTestProperties config = new IntegrationTestProperties();
-        final String grunnbokUser = config.getGrunnbokTinglysingUsername();
-        final String grunnbokPassword = config.getGrunnbokTinglysingPassword();
         if (innsendingServiceWS == null) {
             try {
-                innsendingServiceWS = new DefaultInnsendingServiceWS(grunnbokUser, grunnbokPassword,
+                innsendingServiceWS = new DefaultInnsendingServiceWS(
+                    config.getGrunnbokTinglysingUsername(),
+                    config.getGrunnbokTinglysingPassword(),
                     config.getGrunnbokInnsendingServiceUrl());
             } catch (Throwable t) {
                 t.printStackTrace();
